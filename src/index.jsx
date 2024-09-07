0dab4121788b8b876f5d7430f98f1154baad2cf1
Jaouher Belhadj <jaouherbelhadj2@gmail.com>
2024-09-07 23:00:03 +0100

    implement context to change app theme and get the results of the survey

--- src/index.jsx
+++ src/index.jsx
@@ -7,27 +7,28 @@ import Header from './components/Header';
 import Error from './components/Error';
 import Results from './pages/Results';
 import Freelancers from './pages/Freelancers';
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-   div {
-      font-family: 'Trebuchet MS', Helvetica, sans-serif;
-   }
-`
+import { ThemeProvider, SurveyProvider } from './utils/context';
+import GlobalStyle from './utils/style/GlobalStyle';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/survey/:questionNumber" element={<Survey />} />
-        <Route path='/results' element={<Results />} />
-        <Route path='/freelancers' element={<Freelancers />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/survey/:questionNumber" element={<Survey />} />
+            <Route path='/results' element={<Results />} />
+            <Route path='/freelancers' element={<Freelancers />} />
+            <Route path='*' element={<Error />} />
+          </Routes>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
