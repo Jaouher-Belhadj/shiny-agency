d90568666987b665608142a7e23620b6cca9905b
Jaouher Belhadj <jaouherbelhadj2@gmail.com>
2024-09-08 21:38:02 +0100

    tests Results page functions (Jest)

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -52,7 +52,14 @@ const LoaderWrapper = styled.div`
   justify-content: center;
 `
 
-function formatFetchParams(answers) {
+export function formatJobList(title, listLength, index) {
+  if (index === listLength - 1) {
+    return title
+  }
+  return `${title},`
+}
+
+export function formatFetchParams(answers) {
     const answerNumbers = Object.keys(answers)
 
     return answerNumbers.reduce((previousParams, answerNumber, index) => {
@@ -90,8 +97,7 @@ function Results() {
                   key={`result-title-${index}-${result.title}`}
                   theme={theme}
                 >
-                  {result.title}
-                  {index === resultsData.length - 1 ? '' : ','}
+                  {formatJobList(result.title, resultsData.length, index)}
                 </JobTitle>
               ))}
           </ResultsTitle>
---
+++ src/pages/Results/index.test.js
@@ -0,0 +1,24 @@
+import { formatJobList, formatFetchParams } from './'
+
+describe('The formatJobList function', () => {
+    it('should add a comma to a word', () => {
+        const expectedState = 'item2,'
+        expect(formatJobList('item2', 3, 1)).toEqual(expectedState)
+    })
+
+    it('should not add a comma to the last element of the list', () => {
+        const expectedState = 'item3'
+        expect(formatJobList('item3', 3, 2)).toEqual(expectedState)
+    })
+})
+
+describe('The formatFetchParams function', () => {
+    it('should use the right format for param', () => {
+        const expectedState = 'a1=answer1'
+        expect(formatFetchParams({ 1: 'answer1' })).toEqual(expectedState)
+    })
+    it('should concatenate params with an &', () => {
+        const expectedState = 'a1=answer1&a2=answer2&a3=answer3'
+        expect(formatFetchParams({ 1: 'answer1', 2: 'answer2', 3: 'answer3' })).toEqual(expectedState)
+    })
+})
